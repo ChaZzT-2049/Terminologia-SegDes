c73a8bac2652f0ccf320ce3537241895a58c0f03
ChaZzT-2049 <2005041@utrivieramaya.edu.mx>
2023-05-23 09:55:10 -0500

    agregado titulo de concepto e indicador en boton

--- fetch.js
+++ fetch.js
@@ -14,19 +14,27 @@ function loadInfo() {
         //crea los conceptos por unidad
         let terms1 = "";
         let terms2 = "";
+        //
+        let titulos1 = "";
+        let titulos2 = "";
         //obtener los botones de cada unidad mandando data y el valor de la unidad
         buttons1 = getButtons(data, 1);
         buttons2 = getButtons(data, 2);
         //obtener los conceptos de cada unidad mandando data y el valor de la unidad
         terms1 = getConcepts(data, 1);
         terms2 = getConcepts(data, 2);
-        
+        //
+        titulos1 = getTitles(data, 1);
+        titulos2 = getTitles(data, 2);
         //agregar los botones de cada unidad al html
         document.getElementById('buttons-u1').innerHTML = buttons1;
         document.getElementById('buttons-u2').innerHTML = buttons2;
         //agregar los conceptos de cada unidad al html
         document.getElementById('concepts-u1').innerHTML = terms1;
         document.getElementById('concepts-u2').innerHTML = terms2;
+        //
+        document.getElementById('titulos-u1').innerHTML = titulos1;
+        document.getElementById('titulos-u2').innerHTML = titulos2;
     }
 }
 //funcion obtener botones recibe la data y la unidad
@@ -38,7 +46,7 @@ function getButtons(data, unidad){
         //si el elemento pertenece a la unidad enviada entonces se crea un boton con los datos del elemento
         if(element.unidad == unidad){
             //el elemento llama a una funcion que dibujara la definicion en la pantalla con cada click
-            buttons += `<button onclick="display(${element.id},${element.unidad})" class="btn">${element.termino}</button>`;
+            buttons += `<button id="${element.id}" onclick="display(${element.id},${element.unidad})" class="btn">${element.termino}</button>`;
         }
     });
     //se regresan los datos creados
@@ -54,6 +62,21 @@ function getConcepts(data, unidad){
         }
     });
     return terms
+}
+//
+function getTitles(data, unidad){
+    //variable que almacena los botones
+    let titulos = ""
+    //recorrer la data
+    data.forEach(element => {
+        //si el elemento pertenece a la unidad enviada entonces se crea un boton con los datos del elemento
+        if(element.unidad == unidad){
+            //el elemento llama a una funcion que dibujara la definicion en la pantalla con cada click
+            titulos += `<h5 id="${element.id}" class="titulo-concepto">${element.termino}</h5>`;
+        }
+    });
+    //se regresan los datos creados
+    return titulos
 }
 //llamada a la funcion principal
 loadInfo();
--- script.js
+++ script.js
@@ -1,43 +1,65 @@
 //obtener los contenedores de los conceptos
 const u1 = document.getElementById("concepts-u1");
 const u2 = document.getElementById("concepts-u2");
+//
+const tu1 = document.getElementById("titulos-u1");
+const tu2 = document.getElementById("titulos-u2");
+//
+const btns1 = document.getElementById("buttons-u1");
+const btns2 = document.getElementById("buttons-u2");
 //funcion para dibujar el concepto seleccionado
 function display(id,unidad){
     //evaluar la unidad a la que pertence el elemento
     if(unidad == 1){
         //ocultar el contenedor de la unidad contraria
         u2.style.display = "none";
+        tu2.style.display = "none";
         //obtener los conceptos de la unidad correspondiente
         let elements = u1.getElementsByClassName("parrafo");
+        let titles = tu1.getElementsByClassName("titulo-concepto");
+        let btns = btns1.getElementsByClassName("btn");
         //recorrer los conceptos
         for(i = 0; i < elements.length; i++){
             //evaluar si el concepto recorrido es el mismo que mando a llamar el boton con su id
             if(elements[i].id == id){
                 //dibujar el concepto en pantalla
                 elements[i].style.display = "block";
+                titles[i].style.display = "block";
+                btns[i].style.backgroundColor = "#D8DFE8";
             }else{
                 //si no es el mismo entonces oculta ese elemento recorrido
                 elements[i].style.display = "none";
+                titles[i].style.display = "none";
+                btns[i].style.backgroundColor = "#FFFFFF";
             }
         }
         //finalmente dibuja el contenedor de la unidad
         u1.style.display = "block";
+        tu1.style.display = "block";
     }else{
         //ocultar el contenedor de la unidad contraria
         u1.style.display = "none";
+        tu1.style.display = "none";
         //obtener los conceptos de la unidad correspondiente
         let elements = u2.getElementsByClassName("parrafo");
+        let titles = tu2.getElementsByClassName("titulo-concepto");
+        let btns = btns2.getElementsByClassName("btn");
         //recorrer los conceptos
         for(i = 0; i < elements.length; i++){
             //evaluar si el concepto recorrido es el mismo que mando a llamar el boton con su id
             if(elements[i].id == id){
                 elements[i].style.display = "block";
+                titles[i].style.display = "block"
             }else{
                 elements[i].style.display = "none";
+                titles[i].style.display = "none";
+                btns[i].style.backgroundColor = "#D8DFE8";
+                btns[i].style.backgroundColor = "#FFFFFF";
             }
         }
         
         u2.style.display = "block";
+        tu2.style.display = "block";
     }
     
 }
