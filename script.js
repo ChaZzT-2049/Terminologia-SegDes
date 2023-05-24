c4851735fcf7d1d27518920dbc364f472ae32ad7
ChaZzT-2049 <2005041@utrivieramaya.edu.mx>
2023-05-24 16:11:50 -0500

    Modificando estilos btn con js

--- script.js
+++ script.js
@@ -25,12 +25,14 @@ function display(id,unidad){
                 //dibujar el concepto en pantalla
                 elements[i].style.display = "block";
                 titles[i].style.display = "block";
-                btns[i].style.backgroundColor = "#D8DFE8";
+                btns[i].style.backgroundColor = "#3e5ec9";
+                btns[i].style.color = "#FFFFFF";
             }else{
                 //si no es el mismo entonces oculta ese elemento recorrido
                 elements[i].style.display = "none";
                 titles[i].style.display = "none";
                 btns[i].style.backgroundColor = "#FFFFFF";
+                btns[i].style.color = "#3e5ec9";
             }
         }
         //finalmente dibuja el contenedor de la unidad
@@ -40,6 +42,7 @@ function display(id,unidad){
         //ocultar el contenedor de la unidad contraria
         u1.style.display = "none";
         tu1.style.display = "none";
+
         //obtener los conceptos de la unidad correspondiente
         let elements = u2.getElementsByClassName("parrafo");
         let titles = tu2.getElementsByClassName("titulo-concepto");
@@ -49,12 +52,15 @@ function display(id,unidad){
             //evaluar si el concepto recorrido es el mismo que mando a llamar el boton con su id
             if(elements[i].id == id){
                 elements[i].style.display = "block";
-                titles[i].style.display = "block"
+                titles[i].style.display = "block";
+                btns[i].style.backgroundColor = "#3e5ec9";
+                btns[i].style.color = "#FFFFFF";
             }else{
                 elements[i].style.display = "none";
                 titles[i].style.display = "none";
-                btns[i].style.backgroundColor = "#D8DFE8";
+                
                 btns[i].style.backgroundColor = "#FFFFFF";
+                btns[i].style.color = "#3e5ec9";
             }
         }
         
